aba1e660d8cb70905dfc2f5179188e8b17f36612
Sudip Pandore <sudippandore9890@gmail.com>
2026-07-29 19:10:39 +0530

    reverse number

---
+++ 3digitnumber.c
@@ -0,0 +1,26 @@
+//program to Accept a 3 digit number from user and find the sum of the digits and also reverse the number
+
+void reverse();
+#include<stdio.h>
+void main(){
+    printf("start\n");
+    reverse();
+    printf("\nend");
+}
+
+void reverse() {
+    int num, r1,r2,r3,x ,sum;
+    printf("enter a 3 digit number: ");
+    scanf("%d", &num); 
+
+    r1 = num % 10;
+    x = num / 10;
+    r2 = x % 10;
+    r3 = x / 10;
+
+    sum = r1 + r2 + r3;
+
+    printf("sum = %d\n", sum);
+    printf("reversed num = %d%d%d", r1,r2,r3);
+
+}
