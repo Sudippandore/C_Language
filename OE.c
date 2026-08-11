61439ea1d1a7fdf5fead9014789c1b02ae45349b
Sudip Pandore <sudippandore9890@gmail.com>
2026-08-11 19:44:40 +0530

    Odd & even

---
+++ OE.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int main(){
+ 	int n;  //dabba ban gaya
+ 	printf("Enter a number:");
+ 	scanf("%d",&n);//dabba me value daal di,n=7
+ 	if(n%2==0){ //even
+ 		printf("Even number");
+ 	}
+ 	
+ 	if(n%2!=0) { //odd
+ 		printf("Odd Number");
+	 }
+	
+ 	
+ 	
+ 	
+ 	
+ 	
+      return 0;
+    }
+	
+	
+
